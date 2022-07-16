96ca043816a3aabd53e47d6131ea3b95b09dbe89
Dmitrii <develop.kosarev@gmail.com>
2022-07-16 19:22:12 +0200

    Adding b5

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -2,21 +2,18 @@
 //= ../../../node_modules/jquery/dist/jquery.js
 
 // Import Popper
-//= ../../../node_modules/popper.js/dist/umd/popper.js
+////= ../../../node_modules/popper.js/dist/umd/popper.js
 
-// Import Bootstrap 4
-//= ../../../node_modules/bootstrap/js/dist/util.js
-//= ../../../node_modules/bootstrap/js/dist/alert.js
+// Import Bootstrap 5
+////= ../../../node_modules/bootstrap/dist/js/bootstrap.js
+
+//= ../../../node_modules/bootstrap/js/dist/dom/data.js
+//= ../../../node_modules/bootstrap/js/dist/dom/event-handler.js
+//= ../../../node_modules/bootstrap/js/dist/dom/manipulator.js
+//= ../../../node_modules/bootstrap/js/dist/dom/selector-engine.js
+//= ../../../node_modules/bootstrap/js/dist/base-component.js
 //= ../../../node_modules/bootstrap/js/dist/button.js
-//= ../../../node_modules/bootstrap/js/dist/carousel.js
 //= ../../../node_modules/bootstrap/js/dist/collapse.js
-//= ../../../node_modules/bootstrap/js/dist/dropdown.js
-//= ../../../node_modules/bootstrap/js/dist/modal.js
-//= ../../../node_modules/bootstrap/js/dist/tooltip.js
-//= ../../../node_modules/bootstrap/js/dist/popover.js
-//= ../../../node_modules/bootstrap/js/dist/scrollspy.js
-//= ../../../node_modules/bootstrap/js/dist/tab.js
-//= ../../../node_modules/bootstrap/js/dist/toast.js
 
 // Lazysizes
 //= ../../../node_modules/lazysizes/lazysizes.js
