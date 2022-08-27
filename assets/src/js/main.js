f5b6a48184593e09576b4edda58a25e45bec4ff8
Dmitrii Kosarev <develop.kosarev@gmail.com>
2022-08-27 21:32:55 +0200

    Fixed js

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -5,15 +5,15 @@
 ////= ../../../node_modules/popper.js/dist/umd/popper.js
 
 // Import Bootstrap 5
-////= ../../../node_modules/bootstrap/dist/js/bootstrap.js
+//= ../../../node_modules/bootstrap/dist/js/bootstrap.js
 
-//= ../../../node_modules/bootstrap/js/dist/dom/data.js
-//= ../../../node_modules/bootstrap/js/dist/dom/event-handler.js
-//= ../../../node_modules/bootstrap/js/dist/dom/manipulator.js
-//= ../../../node_modules/bootstrap/js/dist/dom/selector-engine.js
-//= ../../../node_modules/bootstrap/js/dist/base-component.js
-//= ../../../node_modules/bootstrap/js/dist/button.js
-//= ../../../node_modules/bootstrap/js/dist/collapse.js
+////= ../../../node_modules/bootstrap/js/dist/dom/data.js
+////= ../../../node_modules/bootstrap/js/dist/dom/event-handler.js
+////= ../../../node_modules/bootstrap/js/dist/dom/manipulator.js
+////= ../../../node_modules/bootstrap/js/dist/dom/selector-engine.js
+////= ../../../node_modules/bootstrap/js/dist/base-component.js
+////= ../../../node_modules/bootstrap/js/dist/button.js
+////= ../../../node_modules/bootstrap/js/dist/collapse.js
 
 // Lazysizes
 //= ../../../node_modules/lazysizes/lazysizes.js
