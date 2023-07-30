32150def8001bf68422941ccd6d479f9a954fabe
developkosarev <develop.kosarev@gmail.com>
2023-07-30 20:58:14 +0200

    Add webpack

--- gulpfile.js
+++ gulpfile.js
@@ -36,10 +36,26 @@ var config = {
     notify: false
 };
 
-let webpackConfig = {
+let webpackConfig = {    
     output: {
 		filename: '[name].bundle.js',
 	},
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }]
+                        ]
+                    }
+                }
+            }
+        ]
+    },    
 
     mode: 'production'
 }
